c110538ab8df1048648854138bbf5bbb54bbb144
udemonia <brandon.l.lambert@icloud.com>
2021-10-20 06:46:22 -0500

    server working

---
+++ users/schema/schema.js
@@ -0,0 +1,72 @@
+//? The Schema communicates the type of data over to GraphQL 
+//? As well as the relationship between types
+
+const graphql = require('graphql');
+const _ = require('lodash');
+
+// destructure 
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLSchema
+} = graphql
+
+
+//? temp... HARD CODED....
+const users = [
+    {
+        id: '23',
+        firstName: "Brandon",
+        age: 38
+    },
+    {
+        id: '47',
+        firstName: 'Kal',
+        age: 6
+    }
+]
+
+//* this reminds me of Mongoose schemas - 
+        //!  if it was typed
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString } ,
+        age: { type: GraphQLInt }
+    }
+})
+
+//! THE ROOT QUERY
+//? we need to give GraphQL the entry point of the query - in our course example
+//? give me user : 23, all their friends, all of their friends companies....
+//* User : 23 - would be the ROOT QUERY
+
+
+//? You can read this as saying, you can give me a user - if you pass me the id,
+//? I'll give you the UserType back to you....
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString }},
+            //! Resolve, goes into the database and finds the data we're looking for
+            //! ParentValue - not really used ever
+            //! args is the object that gets called with the args in the query...
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
+})
+
+//! takes in the root query and returns a GraphQL Schema instance
+//! Similar to mongoose schema instances
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
+ 
--- users/server.js
+++ users/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 // by convention, we always use GraphQL casing
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 
 //* add middleware to reroute any /graphql routes to an instance of express-graphql
 app.use('/graphql', graphqlHTTP({
+    //? pass the schema into our middleware
+    schema: schema,
     // graphiql for dev env
     graphiql: true
 }))
